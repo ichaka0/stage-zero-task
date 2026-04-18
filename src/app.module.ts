6a1f117cfb4fe8ed35a79b2bc258edcae5c9e019
Nnubia Obinna <nnubiaobinna17@outlook.com>
2026-04-18 02:54:49 +0100

    fix: db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,12 +16,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        // host: configService.get<string>('DB_HOST'),
         url: configService.get<string>('DB_URL'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
         autoLoadEntities: true,
         synchronize: true,
         ssl: {
